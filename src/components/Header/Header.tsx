ac9ab5d5b1aa27cdfe4fe790c92dda188aec6da3
Lőrintz Levente <lorintz.levente@burg.lo.hu>
2025-04-02 09:13:01 +0200

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,62 @@
-import React from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import './header.css'
+import 'remixicon/fonts/remixicon.css'
 
 const Header = () => {
 
-  
-    {/*Hozz létre egy boolean állapotváltozót. Értéke alapján hozzáadja a nav_menu osztállyal ellátott elemhez a show-menu osztályt.*/}
 
-    {/*Hozz létre egy állapotváltozót boolean típussal, amely azt figyeli, hogy kell-e árnyékot állítani a headernek. */}
+    {/*Hozz létre egy boolean állapotváltozót. Értéke alapján hozzáadja a nav_menu osztállyal ellátott elemhez a show-menu osztályt.*/ }
 
-    {/* Ha 50 px-rel lejjebb görgetünk (window.scrollY >= 50), akkor a header osztályú elemhez adjuk hozzá a shadow-header osztályt. */}
+    {/*Hozz létre egy állapotváltozót boolean típussal, amely azt figyeli, hogy kell-e árnyékot állítani a headernek. */ }
 
-    {/*Generáld le a HOME, POPULAR, ABOUT US, PRODUCTS, CONTACT listaelemeket. Mindegyik a megfelelő helyre mutasson az oldalon*/}
-        
-    {/*A szükséges css osztályokat megtalálod a header.css - ben.*/}
+    {/* Ha 50 px-rel lejjebb görgetünk (window.scrollY >= 50), akkor a header osztályú elemhez adjuk hozzá a shadow-header osztályt. */ }
 
-    {/*importáld a remixicont: import "remixicon/fonts/remixicon.css"; */}
+    {/*Generáld le a HOME, POPULAR, ABOUT US, PRODUCTS, CONTACT listaelemeket. Mindegyik a megfelelő helyre mutasson az oldalon*/ }
 
-    {/*close ikon osztálya: ri-close-large-line */}
+    {/*A szükséges css osztályokat megtalálod a header.css - ben.*/ }
 
-    {/*toggle ikon osztálya: ri-apps-2-fill */}
+    {/*importáld a remixicont: import "remixicon/fonts/remixicon.css"; */ }
 
-    return()
+    {/*close ikon osztálya: ri-close-large-line */ }
+
+    {/*toggle ikon osztálya: ri-apps-2-fill */ }
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [shadow, setShadow] = useState(false)
+
+    const handleShowMenu = (b:boolean)=>{
+        setShowMenu(b)
+    }
+
+    useEffect(()=>{
+        const handleScroll = ()=>{
+            setShadow(window.scrollY>=50)
+        }
+        window.addEventListener("scroll", handleScroll)
+    })
+
+    return (
+        <div className={`header ${shadow ? "shadow-header" : ""}`}>
+            <nav>
+                <a href='#' className='nav__logo'>STARCOFFEE</a>
+                <div className={`nav__menu ${showMenu?"show-menu":""}`}>
+                    <ul className='nav__list'>
+                        {["HOME", "POPULAR", "ABOUT US", "PRODUCTS", "CONTACT"].map(item => (
+                            <li>
+                                <a href={`#${item.toLowerCase().replace(" ","")}`} className='nav__link'>{item}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div onClick={()=>handleShowMenu(false)} className='nav__close'>
+                        <i className='ri-close-large-line'></i>
+                    </div>
+                </div>
+                <div onClick={()=>handleShowMenu(true)} className='nav__toggle'>
+                    <i className='ri-apps-2-fill'></i>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { homeImages } from "../../data/data"
+import './home.css'
 
 const Home = () => {
     {/*A komponens megírásához szükséges css osztályokat a home.css-ben találod.*/}
@@ -9,7 +10,24 @@ const Home = () => {
 
   {/*A gomb mutasson az about részre */}
   return (
-    <div>Home</div>
+    <div className="home" id="home">
+      <h1 className="home__title">COLd COFFEE</h1>
+      <div className="home__images">
+        <div className="home__shape"></div>
+        {homeImages.map(image=>(
+          <img src={image.src} alt={image.alt}  className={image.className}/>
+        ))}
+      </div>
+      <img src="/img/home-sticker.svg" alt="" className="home__Sticker"/>
+      <div className="home__data">
+        <p className="home__description">
+          Find delicious hot and cold coffees with the 
+          best varieties, calm the pleasure and enjoy 
+          a good coffee, order now.
+        </p>
+        <a href="#about" className="button">Larn More</a>
+      </div>
+    </div>
   )
 }
 
